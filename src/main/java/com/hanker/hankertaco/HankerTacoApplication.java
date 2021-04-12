0afc43c98a2f109dab9c95b475e63c9aae28e462
han-ker <hanjaeok17@gmail.com>
2021-04-12 23:30:38 +0900

    2021. 04. 12 Hanker - Taco Web Study Start

---
+++ src/main/java/com/hanker/hankertaco/HankerTacoApplication.java
@@ -0,0 +1,13 @@
+package com.hanker.hankertaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankerTacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HankerTacoApplication.class, args);
+    }
+
+}
